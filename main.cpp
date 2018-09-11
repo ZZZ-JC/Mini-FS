7e576715d6dbdeef5b5a3a8258eadffd246194f8
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-09-11 16:30:07 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -43,25 +43,36 @@ char filename[50];
 
 int main()
 {
+	printf("Welcome to Mini-FS file system.\nFor help,use command: ""help"".\nOr type in your command directly.\n\n");
 	while (1) {
+		printf("Mini-FS-->");//???
 		cin >> cmd;
 		if (cmd == "create") {
+			printf("\n");
 			//cin >> SName;
 			creat();
+			printf("\n");
 		}
 		else if (cmd == "mount"){
+			printf("\n");
 			mount();
+			printf("\n");
 		}
 		else if (cmd == "copyin"){
+			printf("\n");
 			//cout<<""//???
 			cin >> SName;
 			copyin(SName);
+			printf("\n");
 		}
 		else if (cmd == "copyout"){
+			printf("\n");
 			cin >> filename >> SName;//???
 			copyout(filename, SName);
+			printf("\n");
 		}
 		else if (cmd == "rename") {
+			printf("\n");
 			char oldName[20];
 			char newName[20];
 			printf("Please input file name which is going to be changed.\n");
@@ -69,20 +80,28 @@ int main()
 			printf("Please input a new file name.\n");
 			scanf("%s", newName);
 			changeName(oldName, newName);
+			printf("\n");
 		}
 		else if (cmd == "help") {
+			printf("\n");
 			help();
+			printf("\n");
 		}
 		else if (cmd == "att") {
+			printf("\n");
 			char fileName[20];
 			printf("Please input a file name to dispaly its attribute.\n");
 			scanf("%s", fileName);
 			Att(fileName);
+			printf("\n");
 		}
 		else if (cmd == "fmt") {
+			printf("\n");
 			fmt();
+			printf("\n");
 		}
 		else if (cmd == "newfile") {
+			printf("\n");
 			char fileName[20];
 			int fileSize;//输入的是文件字节数
 			printf("Please input new file`s name.\n");
@@ -90,12 +109,15 @@ int main()
 			printf("Please input file size.\n");
 			scanf("%d", fileSize);
 			newFile(fileName, fileSize / block_size);
+			printf("\n");
 		}
-		else if (cmd == "deletefile") {
+		else if (cmd == "delete") {
+			printf("\n");
 			char fileName[20];
 			printf("Please input file name to delete a file.\n");
 			scanf("%s", fileName);
 			deleteFile(fileName);
+			printf("\n");
 		}
 	}
 }
